1b097c67025884c662d833883ddc10783a1606df
psourabh311 <psourabh0311@gmail.com>
2026-05-19 02:41:20 +0530

    feat: trigger first CI/CD deployment

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ APP_VERSION = os.environ.get('APP_VERSION', 'v1.0.0')
 def home():
     logger.info("Home endpoint hit")
     return jsonify({
-        'message': 'Flask CI/CD App is running!',
+        'message': 'Flask CI/CD App is running! - Deployed via GitHub Actions 🚀',
         'version': APP_VERSION,
         'status': 'success'
     }), 200
